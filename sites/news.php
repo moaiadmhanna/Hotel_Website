7531e6e34780edd5612ca97d3ed047f7bc9bf80e
if23b191 <if23b191@technikum-wien.at>
2023-11-22 03:06:06 +0100

    new feauture to news

--- sites/news.php
+++ sites/news.php
@@ -10,26 +10,25 @@
         include_once "navbar.php";
 ?>
 <img src="./styles/fotos/parisi-udvar-hotel-budapest-exterior-night-2.webp" class="d-block w-100" style="height:70vh;" alt="Unser Hotel">
-<p class="newsP mt-5" style="text-align:center; font-family: 'Courier New', Courier, monospace; font-size: 24px; font-weight:bolder;">Unser News</p>
-<div class="container my-3">
-    <div class='row'>
-        <div class="card col-md-3 mb-3" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-        <div class="col-md-3 mb-3" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-        <div class="col-md-3 mb-3" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+<p class="newsP mt-5" style="text-align:center; font-family: 'Courier New', Courier, monospace; font-size: 30px; font-weight:bolder;">Unser News</p>
+<div class="container mt-5 pb-5">
+    <div class='row justify-content-around'>
+            <?php
+                foreach($newsData as $news){
+                    $newsHeader=$news["newsHeader"];
+                    $newsBody=$news["newsBody"];
+                    $picture=$news["picture"];
+                    echo "
+                    <div class='card col-4' style='width: 27rem;'>
+                        <img src='$picture' class='card-img' style='height:27rem;' alt='...'>
+                        <div class='card-body' style='border:1px solid gray;'>
+                            <h5 style='text-align:center;'>$newsHeader<h5>
+                            <p class='pt-5 pr-2'>$newsBody</p>
+                        </div>
+                    </div>
+                    ";
+                }
+            ?>
         </div>
     </div>
 </div>
