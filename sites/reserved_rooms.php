ad592499e8a99ac934f8d6c54ea34ccce44d10fc
if23b191 <if23b191@technikum-wien.at>
2023-12-21 03:55:12 +0100

    add stornieren button

--- sites/reserved_rooms.php
+++ sites/reserved_rooms.php
@@ -11,7 +11,7 @@
     ini_set('display_errors', 1);
     // holt die benutzerid vom datenbank, wobei die email adresse = session email adresse.
     $benutzerid = get_user();
-    $sql = "SELECT anreisedatum,abreisedatum,fruehstuck,parkplatz,haustier,gesamtpreis,status,date(reservierungsdatum)
+    $sql = "SELECT anreisedatum,abreisedatum,fruehstuck,parkplatz,haustier,gesamtpreis,status,date(reservierungsdatum),reservierungsdatum
             FROM reservierung WHERE  benutzerid = ?";
     $stmt = $db->prepare($sql);
     $stmt->bind_param("i",$benutzerid);
@@ -33,6 +33,7 @@
                             <th scope='col'>Status</th>
                             <th scope='col'>Gesamtpreis</th>
                             <th scope='col'>Reservierungsdatum</th>
+                            <th scope='col'>Stornieren</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -42,15 +43,36 @@
         while ($row = $result->fetch_assoc()) {
             echo "
                 <tr class='table-warning'>
-                    <th scope='row'>$counter</th>
-                    <td>{$row['anreisedatum']}</td>
-                    <td>{$row['abreisedatum']}</td>
-                    <td>" . ($row['fruehstuck'] ? 'inklusiv' : 'nicht inklusive') . "</td>
-                    <td>" . ($row['parkplatz'] ? 'inklusiv' : 'nicht inklusive') . "</td>
-                    <td>" . ($row['haustier'] ? 'inklusiv' : 'nicht inklusive') . "</td>
-                    <td>{$row['status']}</td>
-                    <td>{$row['gesamtpreis']}</td>
-                    <td>{$row['date(reservierungsdatum)']}</td>
+                    <form method='post'>
+                        <th scope='row'>$counter</th>
+                        <input type='hidden' name='{$row['reservierungsdatum']}'>
+                        <td>{$row['anreisedatum']}</td>
+                        <td>{$row['abreisedatum']}</td>
+                        <td>" . ($row['fruehstuck'] ? 'inklusiv' : 'nicht inklusiv') . "</td>
+                        <td>" . ($row['parkplatz'] ? 'inklusiv' : 'nicht inklusiv') . "</td>
+                        <td>" . ($row['haustier'] ? 'inklusiv' : 'nicht inklusiv') . "</td>
+                        <td>{$row['status']}</td>
+                        <td>{$row['gesamtpreis']}</td>
+                        <td>{$row['date(reservierungsdatum)']}</td>
+                        <td>
+                        <button name='reservierungstornieren' type='submit' style='background: none; border: none;'>
+                            <svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' width='24' height='24' viewBox='0 0 48 48'>
+                                <linearGradient id='wRKXFJsqHCxLE9yyOYHkza_fYgQxDaH069W_gr1' x1='9.858' x2='38.142' y1='9.858' y2='38.142' gradientUnits='userSpaceOnUse'>
+                                    <stop offset='0' stop-color='#f44f5a'></stop>
+                                    <stop offset='.443' stop-color='#ee3d4a'></stop>
+                                    <stop offset='1' stop-color='#e52030'></stop>
+                                </linearGradient>
+                                <circle fill='url(#wRKXFJsqHCxLE9yyOYHkza_fYgQxDaH069W_gr1)' cx='24' cy='24' r='20'></circle>
+                                <path fill='#fff' d='M31.071,15.515l1.414,1.414c0.391,0.391,0.391,1.024,0,1.414L18.343,32.485
+                                    c-0.391,0.391-1.024,0.391-1.414,0l-1.414-1.414c-0.391-0.391-0.391-1.024,0-1.414l14.142-14.142
+                                    C30.047,15.124,30.681,15.124,31.071,15.515z'></path>
+                                <path fill='#fff' d='M32.485,31.071l-1.414,1.414c-0.391,0.391-1.024,0.391-1.414,0L15.515,18.343
+                                    c-0.391-0.391-0.391-1.024,0-1.414l1.414-1.414c0.391-0.391,1.024-0.391,1.414,0l14.142,14.142
+                                    C32.876,30.047,32.876,30.681,32.485,31.071z'></path>
+                            </svg>
+                        </button>
+                        </td>
+                    </form>
                 </tr>
             ";
             $counter++;
