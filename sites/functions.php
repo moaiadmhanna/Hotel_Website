dd96d215d55940e28e864b7e34c0cd757b6c9713
moaiadmhanna <61378172+moaiadmhanna@users.noreply.github.com>
2023-12-21 13:00:09 +0100

    Update functions.php

--- sites/functions.php
+++ sites/functions.php
@@ -11,6 +11,15 @@
         $benutzerid = $row["benutzerid"];
         $stmt->close();
         return $benutzerid;
-    }
+    };
+    function change_status($status){
+         $d = strtotime($_POST["datum"]);
+        $reservierungsdatum = date("Y-m-d H:i:s",$d);
+        $sql = "UPDATE reservierung SET status = $status  WHERE reservierungsdatum = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param("s",$reservierungsdatum);
+        $stmt->execute();
+        $stmt->close();
+    };
 
 ?>
