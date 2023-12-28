dee0304c67b48f2ef95114a35a7af2790ff7bf89
muayad <moaiadmhana999@gmail.comm>
2023-12-28 01:02:51 +0100

    added the icon

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@200;300;400&family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link rel="icon" href="styles/fotos/Else/upper-belvedere-vienna.png" type="image/icon type">
     <link rel ="stylesheet" href="styles/main.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
